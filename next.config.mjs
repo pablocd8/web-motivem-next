3d7605207fe207d9ebe08c73ecfbb6bdb7145129
pablocd8 <pablocerdadonat8@gmail.com>
2026-03-24 17:37:43 +0100

    Fix: ocultar configuración de webpack a Vercel en producción

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    turbopack: {}, 
-    webpack: (config, context) => {
-        // Solo aplicar polling en desarrollo
-        if (context.dev) {
-            config.watchOptions = {
-                poll: 1000,
-                aggregateTimeout: 300,
-            };
-        }
+const nextConfig = {};
+
+// Solo aplicamos la configuración de Webpack para el polling en modo local
+// para que Vercel (Producción) pueda usar Turbopack sin dar errores.
+if (process.env.NODE_ENV === 'development') {
+    nextConfig.webpack = (config, context) => {
+        config.watchOptions = {
+            poll: 1000, // Necesario para el Hot-Reload en Docker Windows
+            aggregateTimeout: 300,
+        };
         return config;
-    },
-};
+    };
+}
 
 export default nextConfig;
